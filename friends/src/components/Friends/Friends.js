66a6975ae967725aad701bc9b7324c985727f50c
deejayeaster <deejayeaster@live.com>
2019-08-20 16:59:29 -0400

    made axios call for friends data

--- friends/src/components/Friends/Friends.js
+++ friends/src/components/Friends/Friends.js
@@ -1,1 +1,29 @@
 // here you  need to call to the server and get  the info. Log it out to see what it looks like. You will also need to create a card for the users and then map over the data applying the card to each friend item.
+
+import React, { useState, useEffect } from "react";
+import axiosWithAuth from "../utils/axiosWithAuth";
+import AddFriend from "./AddFriend";
+import FriendsCard from "./FriendsCard";
+
+const Friends = () => {
+  const [friends, setFriends] = useState([]);
+  useEffect(() => getData(), []);
+  function getData() {
+    axiosWithAuth()
+      .get("http://localhost:5000/api/friends")
+      .then(res => {
+        setFriends(res.data);
+      })
+      .catch(error => console.log(error.res));
+  }
+  return (
+    <div>
+      <AddFriend />
+      {friends.length > 0
+        ? friends.map(friend => <FriendCard key={friend.id} friend={friend} />)
+        : null}
+    </div>
+  );
+};
+
+export default Friends;
